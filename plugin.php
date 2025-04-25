d1705ffe775cbe3c6807c6b30568ec9623b58dcf
Brandon Creed <brandon.f.creed@gmail.com>
2025-04-25 17:51:25 -0400

    initial commit

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Custom Text-Image Slider Block
+ * Description: A Gutenberg block with text and image columns that can slide.
+ * Version: 1.0
+ * Author: Brandon
+ */
+
+function ctsb_register_block() {
+    register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'ctsb_register_block' );
