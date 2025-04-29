905ab597ae4ec69872618682feb6529d9c38bf64
Brandon Creed <brandon.f.creed@gmail.com>
2025-04-29 17:45:33 -0400

    added proper delete button

--- src/index.js
+++ src/index.js
@@ -98,7 +98,18 @@ function Edit({ attributes, setAttributes }) {
                 </div>
                 <div className="slide-image">
                     {slides[currentSlide].imageUrl ? (
-                        <img src={slides[currentSlide].imageUrl} alt={slides[currentSlide].title} />
+                        <div className="image-container">
+                            <img src={slides[currentSlide].imageUrl} alt={slides[currentSlide].title} />
+                            <Button 
+                                className="delete-image-button"
+                                onClick={() => updateSlide(currentSlide, 'imageUrl', '')}
+                                isDestructive
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#FFFFFF">
+                                    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+                                </svg>
+                            </Button>
+                        </div>
                     ) : (
                         <MediaUploadCheck>
                             <MediaUpload
